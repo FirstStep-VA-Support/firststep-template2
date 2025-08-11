c18040da198fb1d21ef2eb41f2e4266df7ff2140
jhrocales <jhrocales@gmail.com>
2025-08-12 07:05:23 +0800

    first commit, firststep va support template boiler

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// typing animation
+
+var typed = new Typed(".typing", {
+   strings:["", "Graphics Designer", "English Teacher", "Social Media Manager", "Virtual Assistant", "Aspiring Voice Actress", "Creative Writer"],
+   typeSpeed: 80,
+   BackSpeed:60,
+   loop:true
+})
